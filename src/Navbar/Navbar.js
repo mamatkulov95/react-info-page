db3e4ce5c7decf0ba702f3eb3eb19c7c130e9eab
Ma'rufjon <m.mamatkulov95@gmail.com>
2022-11-01 14:39:56 +0500

    [Navbar] -Modified Navbar.css and deleted not related files also added google font link

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,14 +1,12 @@
+import ReactLogo from "../images/react-icon-small.png";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="nav">
       <div className="left-nav">
-        <img
-          className="nav-icon"
-          src={require("../images/react-icon-small.png")}
-        ></img>
-        <h3 className="nav-logo_text">ReactFacts</h3>
+        <img className="nav-icon" src={ReactLogo}></img>
+        <h3 className="nav-logo-text">ReactFacts</h3>
       </div>
       <h4 className="nav-title">React Course - Project 1</h4>
     </nav>
